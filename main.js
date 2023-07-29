d75938aa05f671b2196635ef4de1dd46c37a3bd7
shehabsameh1234 <shehabsameh987123@gmail.com>
2023-07-29 21:29:51 +0300

    update

--- main.js
+++ main.js
@@ -11,71 +11,94 @@ let slideTeal = document.getElementById("slide-teal")
 let slideTomato = document.getElementById("slide-tomato")
 let slideBlack = document.getElementById("slide-black")
 var rootCss = document.querySelector(':root')
+var slidepos = document.querySelector('.right-slide-color')
 
 
 
-  
+
 function myFunction_red() {
-    rootCss.style.setProperty('--brown', 'red');
-  }
+  rootCss.style.setProperty('--brown', 'red');
+}
 function myFunction_teal() {
-    rootCss.style.setProperty('--brown', 'teal');
-  }
+  rootCss.style.setProperty('--brown', 'teal');
+}
 function myFunction_tomato() {
-    rootCss.style.setProperty('--brown', 'tomato');
-  }
+  rootCss.style.setProperty('--brown', 'tomato');
+}
 function myFunction_black() {
-    rootCss.style.setProperty('--brown', 'black');
-  }
+  rootCss.style.setProperty('--brown', 'black');
+}
+
+
 
 
 
 window.onscroll = function () {
-    if (scrollY >= 600) {
-        navBar.style.background = "white"
-        navBar.classList.remove("navbar-dark")
-        navBar.classList.add("navbar-light")
-        spanAbout.classList.add("underline-nav")
-        spanOffer.classList.add("underline-nav")
-        spanGallery.classList.add("underline-nav")
-        spanBlog.classList.add("underline-nav")
-        spanContact.classList.add("underline-nav")
-  
+  if (scrollY >= 30) {
 
-    } else {
-        navBar.style.background = "transparent"
-        navBar.classList.add("navbar-dark")
-        spanAbout.classList.remove("underline-nav")
-        spanOffer.classList.remove("underline-nav")
-        spanGallery.classList.remove("underline-nav")
-        spanBlog.classList.remove("underline-nav")
-        spanContact.classList.remove("underline-nav")
-    }
+    slidepos.style.cssText = `
+  position: sticky;
+  top: 50%;
+  left: 100%;
+  
+  `
+
+  } else {
+    slidepos.style.cssText = `
+  position: absolute;
+  top: 55%;
+  right: 0%;
+  `
+  }
+
+  if (scrollY >= 500) {
+    navBar.style.background = "white"
+    navBar.classList.remove("navbar-dark")
+    navBar.classList.add("navbar-light")
+    spanAbout.classList.add("underline-nav")
+    spanOffer.classList.add("underline-nav")
+    spanGallery.classList.add("underline-nav")
+    spanBlog.classList.add("underline-nav")
+    spanContact.classList.add("underline-nav")
+  
+  } else {
+    navBar.style.background = "transparent"
+    navBar.classList.add("navbar-dark")
+    spanAbout.classList.remove("underline-nav")
+    spanOffer.classList.remove("underline-nav")
+    spanGallery.classList.remove("underline-nav")
+    spanBlog.classList.remove("underline-nav")
+    spanContact.classList.remove("underline-nav")
+  
+  }
 }
 
 
+
+
+
 function slider() {
-    if (slideRight.offsetWidth == "200") {
-        slideRight.style.width = "40px"
-        iconSliede.style.bottom = "0%"
-        iconSliede.style.top = "6%"
-        iconSliede.style.right = "15%"
-        slideRed.style.display = "none"
-        slideTeal.style.display = "none"
-        slideTomato.style.display = "none"
-        slideBlack.style.display = "none"
-    } else {
+  if (slideRight.offsetWidth == "200") {
+    slideRight.style.width = "40px"
+    iconSliede.style.bottom = "0%"
+    iconSliede.style.top = "6%"
+    iconSliede.style.right = "15%"
+    slideRed.style.display = "none"
+    slideTeal.style.display = "none"
+    slideTomato.style.display = "none"
+    slideBlack.style.display = "none"
+  } else {
 
-        iconSliede.style.position="relative"
-        iconSliede.style.left = "0%"
-        iconSliede.style.top = "-3PX"
-        slideRight.style.width = "200px"
-        slideRed.style.display = "inline-block"
-        slideTeal.style.display = "inline-block"
-        slideTomato.style.display = "inline-block"
-        slideBlack.style.display = "inline-block"
+    iconSliede.style.position = "relative"
+    iconSliede.style.left = "0%"
+    iconSliede.style.top = "-3PX"
+    slideRight.style.width = "200px"
+    slideRed.style.display = "inline-block"
+    slideTeal.style.display = "inline-block"
+    slideTomato.style.display = "inline-block"
+    slideBlack.style.display = "inline-block"
 
-    }
+  }
 }
 
 
