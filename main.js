25450d640531d149143ec489414669e96db5fc53
shehabsameh1234 <shehabsameh987123@gmail.com>
2023-08-04 23:07:30 +0300

    update

--- main.js
+++ main.js
@@ -96,20 +96,17 @@ function slider() {
 }
 
 
-
-
-
-document.onreadystatechange = function () {
-  if (document.readyState !== "complete") {
-     document.querySelector("body").style.visibility = "hidden";
-     document.getElementById("loading_indicator").style.visibility = "visible";
+// document.onreadystatechange = function () {
+//   if (document.readyState !== "complete") {
+//      document.querySelector("body").style.visibility = "hidden";
+//      document.getElementById("loading_indicator").style.visibility = "visible";
     
-  } else {
-     setTimeout(() => {
-        document.getElementById("loading_indicator").style.display ="none";
-        document.querySelector("body").style.visibility = "visible";
-     }, 2000)
-  }
-};
+//   } else {
+//      setTimeout(() => {
+//         document.getElementById("loading_indicator").style.display ="none";
+//         document.querySelector("body").style.visibility = "visible";
+//      }, 2000)
+//   }
+// };
 
 
