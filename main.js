d1e7488fc117d9c1fb58f1cb6b6c4ea37086fb01
shehabsameh1234 <shehabsameh987123@gmail.com>
2023-08-04 21:16:32 +0300

    update

--- main.js
+++ main.js
@@ -99,3 +99,17 @@ function slider() {
 
 
 
+document.onreadystatechange = function () {
+  if (document.readyState !== "complete") {
+     document.querySelector("body").style.visibility = "hidden";
+     document.getElementById("loading_indicator").style.visibility = "visible";
+    
+  } else {
+     setTimeout(() => {
+        document.getElementById("loading_indicator").style.display ="none";
+        document.querySelector("body").style.visibility = "visible";
+     }, 2000)
+  }
+};
+
+
